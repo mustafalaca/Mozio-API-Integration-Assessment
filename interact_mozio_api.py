edca61cc83e1a0f89154ebeb28949f93895b1be2
Alaca Mustafa <97735752+mustafalaca@users.noreply.github.com>
2023-07-30 07:45:50 +0300

    Update interact_mozio_api.py

--- interact_mozio_api.py
+++ interact_mozio_api.py
@@ -4,24 +4,25 @@ from requests import Response
 from request_parameters import Parameters
 from cheapest_vehicle_with_provider import find_cheapest_from_provider
 
-
 MOZIO_URL = 'Replace with the actual Mozio URL'
+
+headers = Parameters().request_header
+
+session = requests.Session()
+session.headers.update(headers)
+session.cookie = id(session)
 
 
 class Search:
     @staticmethod
     def search() -> str:
-        parameters = Parameters()
-        response: Response = requests.post(url=f'{MOZIO_URL}/search/',
-                                           headers=parameters.request_header,
-                                           json=parameters.search_object)
+        response: Response = session.post(url=f'{MOZIO_URL}/search/',
+                                          json=Parameters().search_object)
         return response.json().get('search_id') if response.json()['results'] != [] else False
 
     @staticmethod
     def search_poll(search_id: str) -> str:
-        parameters = Parameters()
-        response: Response = requests.get(url=f'{MOZIO_URL}/search/{search_id}/poll/',
-                                          headers=parameters.request_header)
+        response: Response = session.get(url=f'{MOZIO_URL}/search/{search_id}/poll/')
         result_id: str = find_cheapest_from_provider(response.json()['results'])
         return result_id
 
@@ -29,31 +30,25 @@ class Search:
 class Reservations:
     @staticmethod
     def reservations(search_id, result_id) -> dict:
-        parameters = Parameters()
-        customer_contact: dict = parameters.customer_contact
+        customer_contact: dict = Parameters().customer_contact
 
         customer_contact['search_id'] = search_id
         customer_contact['result_id'] = result_id
 
-        response: Response = requests.post(url=f'{MOZIO_URL}/reservations/',
-                                           headers=parameters.request_header,
-                                           json=customer_contact)
+        response: Response = session.post(url=f'{MOZIO_URL}/reservations/', json=customer_contact)
         return response.json()
 
     @staticmethod
     def reservation_poll(search_id) -> Response:
-        parameters = Parameters()
-        response: Response = requests.get(url=f'{MOZIO_URL}/reservations/{search_id}/poll/',
-                                          headers=parameters.request_header)
+        response: Response = session.get(url=f'{MOZIO_URL}/reservations/{search_id}/poll/')
         return response.json()
 
 
 class Cancellation:
     @staticmethod
     def cancel(reservation_id) -> int:
-        parameters = Parameters()
-        response: Response = requests.delete(url=f'{MOZIO_URL}/reservations/{reservation_id}/',
-                                             headers=parameters.request_header)
+        response: Response = session.delete(url=f'{MOZIO_URL}/reservations/{reservation_id}/',
+                                            headers=Parameters().request_header)
         return response.status_code
 
 
